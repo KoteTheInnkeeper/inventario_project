5027061fee3b5a0eb1233730389c8434f63c1f48
KoteTheInnkeeper <lucianocorrea58@gmail.com>
2021-02-28 10:30:49 -0300

    Working on the login.

--- blueprints/routes.py
+++ blueprints/routes.py
@@ -3,15 +3,15 @@ from flask import Blueprint, render_template, redirect, url_for
 routes = Blueprint("routes", __name__, static_folder="static", template_folder="template", url_prefix="")
 
 
-@routes.route('/home')
+@routes.route('/home', methods=['POST', 'GET'])
 @routes.route('/')
 def home():
     return render_template("home.html")
 
 
-@routes.route('/test')
-def test2():
-    return "<h1>test</h1>"
+@routes.route('/login', methods=['GET', 'POST'])
+def login():
+    return "<h1>Tasintentando loguear y estás en login.html </h1>"
 
 @routes.route('/epicum')
 def test():
